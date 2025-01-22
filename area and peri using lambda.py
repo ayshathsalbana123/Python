82b408b3b67277be65184ecfb765ceada8b2d74b
ayshathsalbana123 <ayshathsalbana@gmail.com>
2025-01-22 21:34:30 +0530

    Add files via upload

---
+++ area and peri using lambda.py
@@ -0,0 +1,6 @@
+x=lambda l,b:l*b
+y=lambda l,b:2*(l+b)
+l=int(input("enter length of rectangle:"))
+b=int(input("enter breadth of rectangle:"))
+print("area of rectangle is:",x(l,b))
+print("perimeter of rectangle is",y(l,b))
